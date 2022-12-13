1e51da30f4ef8502edd56e340449fd7b7077454d
febin123 <febsep96@gmail.com>
2022-12-13 21:15:33 +0000

    created table and added data through student-list-component

---
+++ student-application/src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -0,0 +1,32 @@
+package net.javaguides.springboot.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.javaguides.springboot.model.Student;
+import net.javaguides.springboot.repository.StudentRepository;
+
+
+
+@RestController
+@RequestMapping("/api/v1/")
+public class StudentController {
+
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	@GetMapping("/students")
+	public List<Student>getAllStudents(){
+		return studentRepository.findAll();
+	}
+//	private StudentRepository studentRepository;
+//	//get all student
+//	@GetMapping("/students")
+//	public List<Student> getAllStudents(){
+//		return studentRepository.findAll();
+//	}
+}
---
+++ student-application/src/main/java/net/javaguides/springboot/model/Student.java
@@ -0,0 +1,59 @@
+package net.javaguides.springboot.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="students")
+public class Student {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name="first_name")
+	private String fistName;
+	@Column(name="last_name")
+	private String lastName;
+	@Column(name="email_id")
+	private String email;
+	
+	public Student() {
+		
+	}
+	
+	public Student(String fistName, String lastName, String email) {
+		super();
+		this.fistName = fistName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFistName() {
+		return fistName;
+	}
+	public void setFistName(String fistName) {
+		this.fistName = fistName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
